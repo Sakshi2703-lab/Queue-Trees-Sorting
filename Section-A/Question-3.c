5ba1338911fd8026137efba0ab6e77b26f6170d2
Sakshi2703-lab <satendrasinghb48@gmail.com>
2025-12-23 12:20:57 +0530

    Create Question-3.c

---
+++ Section-A/Question-3.c
@@ -0,0 +1,35 @@
+ #include <stdio.h> 
+#include <stdlib.h> 
+struct Node { 
+int data; 
+    struct Node *prev, *next;}; 
+struct Node *head = NULL; 
+void insert(int x) 
+ { 
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node)); 
+    n->data = x; 
+    n->next = NULL; 
+   if (head == NULL) { 
+n->prev = NULL; 
+        head = n; 
+        return; 
+    } 
+struct Node *t = head; 
+    while (t->next != NULL) 
+        t = t->next; 
+    t->next = n; 
+    n->prev = t;} 
+void display() {struct Node *t = head; 
+    while (t != NULL) 
+ { 
+        printf("%d <-> ", t->data); 
+        t = t->next;} printf("NULL\n"); 
+} 
+ 
+int main() { 
+insert(10); 
+insert(20); 
+insert(30); 
+display(); 
+return 0; 
+}
